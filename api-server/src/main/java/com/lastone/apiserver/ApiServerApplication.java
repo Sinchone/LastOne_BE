d2cf8d4776f73663cc5c2f19aca233ef33db4669
JeonghoPark <pjhpjh0805@naver.com>
2023-03-09 22:15:44 +0900

    feature: 도메인 생성, 수정 시간 자동 생성 기능 구현

---
+++ api-server/src/main/java/com/lastone/apiserver/ApiServerApplication.java
@@ -0,0 +1,18 @@
+package com.lastone.apiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EntityScan("com.lastone.core")
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.lastone.core"})
+@SpringBootApplication()
+public class ApiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiServerApplication.class, args);
+    }
+}
