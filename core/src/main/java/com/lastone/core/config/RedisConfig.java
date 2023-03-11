3a963fd622b4ff853e3e66cf4a75964dfc26b4ee
JeonghoPark <pjhpjh0805@naver.com>
2023-03-12 03:54:58 +0900

    feat: redis 캐시메모리 설정 클래스 추가
    - refreshToken 저장 및 검증 목적

---
+++ core/src/main/java/com/lastone/core/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.lastone.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+//    @Value("${spring.redis.host}")
+    private String host = "localhost";
+
+//    @Value("${spring.redis.port}")
+    private int port = 6379;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
