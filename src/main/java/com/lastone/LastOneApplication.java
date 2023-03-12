4c1a1a9cc2ca2fb1a468ac4e5cc4a23c5b3ff280
supersett <nolang3748@naver.com>
2023-03-12 19:58:57 +0900

    ãdocs: 수정

--- src/main/java/com/lastone/LastOneApplication.java
+++ src/main/java/com/lastone/LastOneApplication.java
@@ -1,2 +1,13 @@
-package com.lastone;public class LastOneApplication {
+package com.lastone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastOneApplication.class, args);
+    }
+
 }
