2c21acbfdadb9aa14ebe087919364c0c9d07cb21
supersett <nolang3748@naver.com>
2023-03-12 20:28:44 +0900

    docs: 수정

---
+++ chatting-server/src/main/java/com/lastone/chattingserver/ChattingServerApplication.java
@@ -0,0 +1,11 @@
+package com.lastone.chattingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChattingServerApplication.class, args);
+    }
+}
