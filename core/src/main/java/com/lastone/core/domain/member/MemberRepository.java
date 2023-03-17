9e688d1542c767f329692afdb927fb0a8a75fd49
JeonghoPark <pjhpjh0805@naver.com>
2023-03-17 16:33:44 +0900

    fix: member 도메인 변경
    - sbd를 member 도메인에 포함
    - member와 gym 사이에 member_gym 도메인 추가

--- core/src/main/java/com/lastone/core/domain/member/MemberRepository.java
+++ core/src/main/java/com/lastone/core/domain/member/MemberRepository.java
@@ -3,7 +3,9 @@ package com.lastone.core.domain.member;
 import com.lastone.core.domain.gym.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
---
+++ core/src/main/java/com/lastone/core/domain/member_gym/MemberGymRepository.java
@@ -0,0 +1,7 @@
+package com.lastone.core.domain.member_gym;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberGymRepository extends JpaRepository<MemberGym, Long> {
+
+}
