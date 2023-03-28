71999a0c7628a1b33345fe29149983947e20bf26
JeonghoPark <pjhpjh0805@naver.com>
2023-03-28 11:49:12 +0900

    feat: 멤버 도메인 nickname 중복 검증 로직 추가

--- core/src/main/java/com/lastone/core/repository/member/MemberRepository.java
+++ core/src/main/java/com/lastone/core/repository/member/MemberRepository.java
@@ -1,6 +1,5 @@
 package com.lastone.core.repository.member;
 
-import com.lastone.core.domain.gym.Gym;
 import com.lastone.core.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +8,6 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Optional<Member> findByEmail(String email);
+
+    Optional<Member> findByNickname(String nickname);
 }
--- core/src/main/java/com/lastone/core/service/member/MemberService.java
+++ core/src/main/java/com/lastone/core/service/member/MemberService.java
@@ -5,13 +5,13 @@ import com.lastone.core.dto.member.MemberDto;
 import com.lastone.core.dto.member.MemberUpdateDto;
 import com.lastone.core.mapper.mapper.MemberMapper;
 import com.lastone.core.repository.member.MemberRepository;
+import com.lastone.core.service.s3.S3Service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import javax.transaction.Transactional;
-import java.util.List;
+import java.io.IOException;
 import java.util.Optional;
 
 @Slf4j
@@ -24,138 +24,38 @@ public class MemberService {
 
     private final MemberMapper memberMapper;
 
+    private final S3Service s3Service;
 
-//    public MyPageDto findMyPageInfoByMemberId(Long memberId) {
-//
-//        MyPageDto myPageDto = new MyPageDto();
-//
-//        Member member = memberRepository.findById(memberId).orElseThrow(NullPointerException::new);
-//        MemberDto memberDto = memberMapper.toDto(member);
-//        myPageDto.setMember(memberDto);
-//
-//        List<MemberGym> memberGyms = memberGymRepository.findAllByMemberId(memberId);
-//        if (memberGyms.size() != 0) {
-//            for (MemberGym memberGym : memberGyms) {
-//                Long gymId = memberGym.getGymId();
-//                Gym gym = gymRepository.findById(gymId).orElseThrow(NullPointerException::new);
-//                log.info("gymEntity ={}", gym);
-//                GymDto gymDto = gymMapper.toDto(gym);
-//                myPageDto.registerGym(gymDto);
-//            }
-//        }
-//
-//        Sbd sbd = sbdRepository.findLatestRecordByMemberId(memberId);
-//        SbdDto sbdDto = sbdMapper.toDto(sbd);
-//        myPageDto.setSbd(sbdDto);
-//
-//        return myPageDto;
-//    }
-//
-//    public void checkIdentity(Long memberId, Long memberDtoId) {
-//        if (!memberId.equals(memberDtoId)) {
-//            throw new IllegalArgumentException("업데이트 권한이 없습니다.");
-//        }
-//    }
-//
-//    public void updateMyPage(MyPageUpdateDto myPageUpdateDto) {
-//        updateMember(myPageUpdateDto);
-//        updateGym(myPageUpdateDto);
-//    }
-//
-//    private void updateGym(MyPageUpdateDto myPageUpdateDto) {
-//        List<MemberGym> memberGyms = memberGymRepository.findAllByMemberId(myPageUpdateDto.getMemberId());
-//        GymDto gym1 = new GymDto(myPageUpdateDto.getGym1Name(), myPageUpdateDto.getGym1location(), myPageUpdateDto.getGym1latitude(), myPageUpdateDto.getGym1longitude());
-//        GymDto gym2 = new GymDto(myPageUpdateDto.getGym2Name(), myPageUpdateDto.getGym2location(), myPageUpdateDto.getGym2latitude(), myPageUpdateDto.getGym2longitude());
-//
-//        for (MemberGym memberGym : memberGyms) {
-//            Gym gym = gymRepository.findById(memberGym.getGymId()).orElse(null);
-//            Gym gym1 = gymRepository.findByGymDto(gym1);
-//            if (gym1 == null && StringUtils.hasText(myPageUpdateDto.getGym1Name())) {
-//                gymRepository.save(Gym.builder()
-//                        .name())
-//
-//            }
-//
-//            if (isEqualToEachOther(gym, myPageUpdateDto)) {
-//                continue;
-//            }
-//
-//
-//
-//        }
-//    }
-//
-//    private boolean isEqualToEachOther(Gym gym, MyPageUpdateDto myPageUpdateDto) {
-//
-//        if (isEqualToGym1(gym, myPageUpdateDto)) {
-//            return true;
-//        }
-//        if (isEqualToGym2(gym, myPageUpdateDto)) {
-//            return true;
-//        }
-//        return false;
-//    }
-//
-//    private boolean isEqualToGym1(Gym gym, MyPageUpdateDto myPageUpdateDto) {
-//        if (!gym.getName().equals(myPageUpdateDto.getGym1Name())) {
-//            return false;
-//        }
-//        if (!gym.getLocation().equals(myPageUpdateDto.getGym1location())) {
-//            return false;
-//        }
-//        if (gym.getLatitude().equals(myPageUpdateDto.getGym1latitude())) {
-//            return false;
-//        }
-//        if (gym.getLongitude().equals(myPageUpdateDto.getGym1longitude())) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//    private boolean isEqualToGym2(Gym gym, MyPageUpdateDto myPageUpdateDto) {
-//        if (!gym.getName().equals(myPageUpdateDto.getGym2Name())) {
-//            return false;
-//        }
-//        if (!gym.getLocation().equals(myPageUpdateDto.getGym2location())) {
-//            return false;
-//        }
-//        if (gym.getLatitude().equals(myPageUpdateDto.getGym2latitude())) {
-//            return false;
-//        }
-//        if (gym.getLongitude().equals(myPageUpdateDto.getGym2longitude())) {
-//            return false;
-//        }
-//        return true;
-//    }
-//
-//
-//    private void updateMember(MyPageUpdateDto myPageUpdateDto) {
-//        Member member = memberRepository.findById(myPageUpdateDto.getMemberId()).orElseThrow(NullPointerException::new);
-//        MemberDto memberDto = MemberDto.builder()
-//                .nickname(myPageUpdateDto.getNickname())
-//                .gender(myPageUpdateDto.getGender())
-//                .workoutDay(myPageUpdateDto.getWorkoutDay())
-//                .workoutTime(myPageUpdateDto.getWorkoutTime())
-//                .workoutPurpose(myPageUpdateDto.getWorkoutPurpose())
-//                .build();
-//        member.updateMember(memberDto);
-//    }
 
     public MemberDto findById(Long memberId) {
         Member member = memberRepository.findById(memberId).orElseThrow(NullPointerException::new);
         return memberMapper.toDto(member);
     }
 
-    public void update(MemberUpdateDto memberUpdateDto, MultipartFile profileImg) {
-        Member member = memberRepository.findById(memberUpdateDto.getId()).orElseThrow(NullPointerException::new);
-        // todo 이미지 파일 처리
-        member.update(memberUpdateDto);
+    public void update(Long memberId, MemberUpdateDto memberUpdateDto, MultipartFile profileImg) throws IOException {
+        Member member = memberRepository.findById(memberId).orElseThrow(NullPointerException::new);
+        isDuplicatedNickname(memberUpdateDto.getNickname(), member.getNickname());
 
+        if (profileImg != null) {
+            s3Service.delete(member.getProfileUrl());
+            memberUpdateDto.setProfileUrl(s3Service.upload(profileImg));
+        }
+        member.update(memberUpdateDto);
+    }
+
+    private void isDuplicatedNickname(String updateNickname, String nickname) {
+        if (updateNickname.equals(nickname)) {
+            return;
+        }
+        Optional<Member> findMember = memberRepository.findByNickname(updateNickname);
+        if (findMember.isPresent()) {
+            throw new IllegalArgumentException("이미 해당 닉네임을 지닌 회원이 존재합니다.");
+        }
     }
 
     public void isExist(Long memberId) {
         Optional<Member> member = memberRepository.findById(memberId);
-        if (member.isPresent()) {
+        if (member.isEmpty()) {
             throw new NullPointerException();
         }
     }
