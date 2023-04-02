1cc8bda44d30974b7eee19835c771c61454d0b6d
JeonghoPark <pjhpjh0805@naver.com>
2023-04-02 12:34:31 +0900

    fix: sbdRepository 테이블 명 오기입 수정

--- core/src/main/java/com/lastone/core/repository/sbd/SbdRepository.java
+++ core/src/main/java/com/lastone/core/repository/sbd/SbdRepository.java
@@ -3,13 +3,14 @@ package com.lastone.core.repository.sbd;
 import com.lastone.core.domain.sbd.Sbd;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface SbdRepository extends JpaRepository<Sbd, Long> {
 
-    @Query(nativeQuery = true, value = "SELECT * FROM Sbd s WHERE s.member_id = :memberId ORDER BY s.sbd_id DESC LIMIT 1")
-    Sbd findLatestRecordByMemberId(Long memberId);
+    @Query(nativeQuery = true, value = "SELECT * FROM sbd s WHERE s.member_id = :memberId ORDER BY s.sbd_id DESC LIMIT 1")
+    Sbd findLatestRecordByMemberId(@Param("memberId") Long memberId);
 
 
 }
