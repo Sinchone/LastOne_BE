ae5197ffb20ee55740f92ffa6d94334ab1a587f4
JeonghoPark <pjhpjh0805@naver.com>
2023-04-02 12:42:59 +0900

    feat: memberGymRepository 소프트 Delete 반영한 find 쿼리 구현

--- core/src/main/java/com/lastone/core/repository/member_gym/MemberGymRepository.java
+++ core/src/main/java/com/lastone/core/repository/member_gym/MemberGymRepository.java
@@ -1,13 +1,13 @@
 package com.lastone.core.repository.member_gym;
 
-import com.lastone.core.domain.gym.Gym;
 import com.lastone.core.domain.member_gym.MemberGym;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.ArrayList;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MemberGymRepository extends JpaRepository<MemberGym, Long> {
 
-    List<MemberGym> findAllByMemberId(Long memberId);
+    @Query("select m from MemberGym m where m.memberId = :memberId and m.isDeleted = false")
+    List<MemberGym> findAllByMemberIdAndDelete(@Param("memberId") Long memberId);
 }
