84f35ee66fb45430956d09a40408cb598f7d7dd2
JeonghoPark <pjhpjh0805@naver.com>
2023-04-02 12:39:39 +0900

    feat: memberGym 도메인 소프트 delete를 위한 칼럼 추가

--- core/src/main/java/com/lastone/core/domain/member_gym/MemberGym.java
+++ core/src/main/java/com/lastone/core/domain/member_gym/MemberGym.java
@@ -1,11 +1,13 @@
 package com.lastone.core.domain.member_gym;
 
 import com.lastone.core.repository.BaseTime;
-import com.sun.istack.NotNull;
+import com.lastone.core.util.BooleanToYNConverter;
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
+@Table(name = "member_gym")
 @Entity
 @Getter
 @Builder
@@ -24,7 +26,15 @@ public class MemberGym extends BaseTime {
     @Column(nullable = false)
     private Long gymId;
 
+    @Convert(converter = BooleanToYNConverter.class)
+    @ColumnDefault("false")
+    private boolean isDeleted;
+
     public void changeGymId(Long id) {
         this.gymId = id;
+    }
+
+    public void delete() {
+        this.isDeleted = true;
     }
 }
