1ca16a0b1f37ed61ff7ce432abba68c0f2e08109
JeonghoPark <pjhpjh0805@naver.com>
2023-04-04 12:00:26 +0900

    feat: memberController에서 MypageController 로 변경

--- api-server/src/main/java/com/lastone/apiserver/controller/MyPageController.java
+++ api-server/src/main/java/com/lastone/apiserver/controller/MyPageController.java
@@ -1,10 +1,9 @@
 package com.lastone.apiserver.controller;
 
 import com.lastone.apiserver.service.mypage.MyPageService;
-import com.lastone.apiserver.service.mypage.MyPageServiceImpl;
 import com.lastone.core.dto.mypage.MyPageDto;
 import com.lastone.core.dto.mypage.MyPageUpdateDto;
-import com.lastone.core.dto.response.BaseResponse;
+import com.lastone.core.dto.response.SuccessResponse;
 import com.lastone.core.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,24 +27,15 @@ public class MyPageController {
         Long memberId = userDetails.getId();
         MyPageDto myPage = myPageService.getMyPage(memberId);
 
-        BaseResponse baseResponse = BaseResponse.builder()
-                .message("마이페이지 조회에 성공하였습니다.")
-                .data(myPage)
-                .build();
-
-        return ResponseEntity.ok().body(baseResponse);
+        return ResponseEntity.ok().body(new SuccessResponse("마이페이지 정보조회에 성공하였습니다.", myPage));
     }
+
 
     @GetMapping("{memberId}")
     public ResponseEntity<Object> getMyPageByMemberId(@PathVariable Long memberId) {
         MyPageDto myPage = myPageService.getMyPage(memberId);
 
-        BaseResponse baseResponse = BaseResponse.builder()
-                .message("마이페이지 조회에 성공하였습니다.")
-                .data(myPage)
-                .build();
-
-        return ResponseEntity.ok().body(baseResponse);
+        return ResponseEntity.ok().body(new SuccessResponse("마이페이지 정보 조회에 성공하였습니다.", myPage));
     }
 
     @PutMapping
@@ -53,29 +43,10 @@ public class MyPageController {
                                                @RequestPart(required = false) @Validated MyPageUpdateDto myPage,
                                                @RequestPart(required = false) MultipartFile profileImg) throws IOException {
         Long memberId = userDetails.getId();
+        log.info("{}, {}, {}",myPage.getMember().toString(), myPage.getGyms().toString(), myPage.getSbd().toString());
         myPageService.updateMyPage(memberId, myPage, profileImg);
 
-        BaseResponse baseResponse = BaseResponse.builder()
-                .message("마이페이지 수정에 성공하였습니다.")
-                .build();
 
-        return ResponseEntity.ok().body(baseResponse);
+        return ResponseEntity.ok().body(new SuccessResponse("마이페이지 수정 작업에 성공하였습니다."));
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
