a0cd4058245f6d8f70cddfa3733ae5f060cb60b0
beetrbgus <devbeetrb0214@gmail.com>
2023-04-06 02:27:15 +0900

    style - 불필요한 Repository 삭제
    ## 한것
    - ChatRoom을 MySQL -> MongoDB로 변경함에 따라서 사용하지 않게 된 ChatRoom의 JPARespotory 제거
    - TestChatMessageController도 현재 개발된 ChatMessageController로 대체가능하기 때문에 삭제

--- core/src/main/java/com/lastone/core/repository/chatroom/ChatRoomRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-package com.lastone.core.repository.chatroom;
-
-import com.lastone.core.domain.chat.ChatRoom;
-import org.springframework.data.domain.Page;
-
-public interface ChatRoomRepositoryCustom {
-    Page<ChatRoom> getList();
-}
