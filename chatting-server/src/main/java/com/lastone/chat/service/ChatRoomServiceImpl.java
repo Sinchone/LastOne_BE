c0c97501d983f7eacd319e381239949e5c4375de
beetrbgus <devbeetrb0214@gmail.com>
2023-04-06 02:40:44 +0900

    feature - 채팅 메세지 비즈니스로직 수정
    ## 한것
    - 채팅방 DTO 변경
      - 본인의 회원번호는 JWT 토큰에 있기 때문에, 다른 사람의 번호만 받아서 사용
    - Service 로직 수정
      - Optional로 예외처리를 할 수 있도록 변경
      - 방 생성시 검색 추가
    - 에러코드 추가
    - 로그인한 유저가 참여자가 아닐 때의 예외 추가

--- chatting-server/src/main/java/com/lastone/chat/service/ChatRoomServiceImpl.java
+++ chatting-server/src/main/java/com/lastone/chat/service/ChatRoomServiceImpl.java
@@ -1,41 +1,67 @@
 package com.lastone.chat.service;
 
-import com.lastone.chat.exception.CannotFountChatRoom;
+import com.lastone.chat.dto.chatroom.ChatRoomListDto;
+import com.lastone.chat.dto.chatroom.ChatRoomResDto;
+import com.lastone.chat.persistence.MessageColumn;
+import com.lastone.chat.persistence.RoomColumn;
 import com.lastone.chat.exception.ChatException;
-import com.lastone.core.repository.ChatRoomRepository;
-import com.lastone.core.domain.chat.ChatRoom;
+import com.lastone.chat.exception.NotParticipantChatRoom;
+import com.lastone.chat.repository.ChatMessageRepository;
+import com.lastone.chat.persistence.ChatRoom;
 import com.lastone.core.domain.chat.ChatStatus;
 import com.lastone.core.dto.chatroom.ChatRoomCreateReqDto;
 import com.lastone.core.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.GroupOperation;
+import org.springframework.data.mongodb.core.aggregation.MatchOperation;
+import org.springframework.data.mongodb.core.aggregation.ProjectionOperation;
+import org.springframework.data.mongodb.core.aggregation.SortOperation;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.group;
 
 @Service
 @RequiredArgsConstructor
 public class ChatRoomServiceImpl implements ChatRoomService {
-    private final ChatRoomRepository chatRoomRepository;
-
+    private final ChatMessageRepository messageRepository;
+    private final MongoTemplate mongoTemplate;
     /**
      * 채팅방을 생성하기 전, 이미 해당 참여자가 있는지 확인
      * 없다면 채팅방 생성
      * 있다면 해당 채팅방의 번호 반환
      */
+    private final String senderId = MessageColumn.SENDERID.getWord();
+    private final String receiverId = MessageColumn.RECEIVERID.getWord();
+    private final String roomId = MessageColumn.ROOMID.getWord();
+    private final String content = MessageColumn.CONTENT.getWord();
+    private final String createdAt = MessageColumn.CREATEDAT.getWord();
+
     @Override
-    public Long createRoom(Long userId, ChatRoomCreateReqDto createReqDto) {
+    public String createRoom(Long userId, ChatRoomCreateReqDto createReqDto) {
         Map<String, Long> userIdMap = userIdSort(userId, createReqDto.getParticipationId());
         Long hostId = userIdMap.get("hostId");
         Long participationId = userIdMap.get("participationId");
 
-        Optional<ChatRoom> chatRoomOptional = chatRoomRepository.findByHostIdAndParticipationId(hostId, participationId);
-
-        if(!chatRoomOptional.isPresent()) {
+        Query query = Query.query(
+                        Criteria.where(RoomColumn.PARTICIPATIONS.getWord())
+                        .all(userIdMap));
+        Optional<ChatRoom> chatRoomOptional = Optional.ofNullable(mongoTemplate.findOne(query, ChatRoom.class));
+        if(chatRoomOptional.isEmpty()) {
             ChatRoom createChatRoom = ChatRoom.create(hostId, participationId);
-            ChatRoom save = chatRoomRepository.save(createChatRoom);
+            ChatRoom save = mongoTemplate.save(createChatRoom);
             return save.getId();
         }else {
             isRoomValidation(chatRoomOptional.get().getStatus());
@@ -45,15 +71,55 @@ public class ChatRoomServiceImpl implements ChatRoomService {
 
     @Override
     @Transactional
-    public void deleteRoom(Long roomId, Long userId) {
-        ChatRoom chatRoom = chatRoomRepository.findById(roomId).orElseThrow(CannotFountChatRoom::new);
-        isRoomValidation(chatRoom.getStatus());
-        if(chatRoom.getParticipationId() != userId
-        || chatRoom.getHostId() != userId) {
-            throw new ChatException(ErrorCode.UNAUTHORIZED);
+    public void deleteRoom(String roomId, Long userId) {
+        Optional<ChatRoom> chatRoomOptional = Optional.ofNullable(mongoTemplate.findById(roomId, ChatRoom.class));
+
+        if(chatRoomOptional.isEmpty()) {
+            throw new ChatException(ErrorCode.NOT_FOUNT_ROOM);
         }
+        ChatRoom chatRoom = chatRoomOptional.get();
+
+        if(chatRoom.getStatus().equals(ChatStatus.DELETED)) {
+            return;
+        }
+        isRoomValidation(chatRoom.getStatus());
+
+        chatRoom.getParticipations().stream()
+                .filter(paticipant -> paticipant == userId )
+                .findFirst().orElseThrow(NotParticipantChatRoom::new);
         chatRoom.delete();
-        chatRoomRepository.save(chatRoom);
+        mongoTemplate.save(chatRoomOptional);
+    }
+
+    @Override
+    public Page<ChatRoomResDto> getList(Long userId, Pageable pageable) {
+        Sort sortCondition = pageable.getSort();
+
+        Query query = new Query();
+        Criteria criteria =
+                new Criteria(senderId)
+                    .is(userId)
+                    .orOperator(
+                        new Criteria(receiverId)
+                    ).is(userId);
+        GroupOperation groupByRoomId = group(roomId).push(content).as(content);
+        ProjectionOperation projection = Aggregation.project() //대상선정
+                .and(roomId).as(roomId)
+                .and(content).as(content)
+                .and(createdAt).as(createdAt);
+
+        SortOperation sort = Aggregation.sort(Sort.by(Sort.Order.desc(createdAt)));
+        MatchOperation matchStage = Aggregation.match(criteria);
+        Aggregation aggregation = Aggregation.newAggregation(
+                groupByRoomId, projection, matchStage, sort
+        );
+        List<ChatRoomListDto> messages = mongoTemplate.aggregate(
+                        aggregation,
+                        "messages",
+                        ChatRoomListDto.class)
+                .getMappedResults();
+
+        return null;
     }
 
     private void isRoomValidation(ChatStatus roomStatus) {
