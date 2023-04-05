ca770da8f6897812e6be0568d3a0743f243fc7b0
beetrbgus <devbeetrb0214@gmail.com>
2023-04-06 02:33:26 +0900

    feature - 채팅방, 메세지 Collection 생성
    ## 한것
    - 채팅방 :
      - MySQL의 Entity -> MongoDB Collection으로 수정.
      - 참여자를 participations 배열로 관리
    - 채팅 메세지 : roomId를 RDBMS에서 Long으로 관리하던 것을 mongo로 사용하기 때문에 String으로 사용

--- chatting-server/src/main/java/com/lastone/chat/persistence/ChatMessage.java
+++ chatting-server/src/main/java/com/lastone/chat/persistence/ChatMessage.java
@@ -2,20 +2,18 @@ package com.lastone.chat.persistence;
 
 import com.lastone.core.dto.message.ChatMessageReqDto;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @Document(value = "messages")
 public class ChatMessage {
     @Id
     private String id;
     private Long senderId;
-    private Long roomId;
+    private String roomId;
     private Long receiverId;
     private String content;
     private boolean isRead;
@@ -23,7 +21,7 @@ public class ChatMessage {
     @CreatedDate
     private LocalDateTime createdAt;
 
-    public ChatMessage(Long roomId, ChatMessageReqDto messageReqDto) {
+    public ChatMessage(String roomId, ChatMessageReqDto messageReqDto) {
         this.senderId = messageReqDto.getSenderId();
         this.roomId = roomId;
         this.receiverId = messageReqDto.getReceiverId();
