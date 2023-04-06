5873180c62d77917fdb334cf14b1d0c8ee5b8060
JeonghoPark <pjhpjh0805@naver.com>
2023-04-06 14:36:35 +0900

    feat: 토큰 발급을 위한 JwtProvider 로직 수정 및 Token 관련 클래스 생성
    <한것>
    - Oauth2 로그인 완료 후 토큰 발급을 위한 JwtProvider 클래스의 토큰 지속 시간을 yml로 부터 가져오는 방식으로 변경하였습니다.
    또한 기존의 Map을 반환하는 방식에서 TokenResponse를 반환하는 방식으로 변경했습니다.
    - accessToken과 refreshToken을 속성으로 가지고 있는 TokenResponse 클래스를 생성했습니다.

--- core/src/main/java/com/lastone/core/security/jwt/JwtProvider.java
+++ core/src/main/java/com/lastone/core/security/jwt/JwtProvider.java
@@ -5,42 +5,47 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.*;
 
 public class JwtProvider {
 
-    private static final int ACCESS_TOKEN_TIME = 3 * 60 * 60 * 1000;
-    private static final int REFRESH_TOKEN_TIME = 24 * 60 * 60 * 1000;
+    @Value("${token.time.access}")
+    private static long accessTokenDuration;
+
+    @Value("${token.time.refresh}")
+    private static long refreshTokenDuration;
+
     private static final String secretKey = "test";
     private static final Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
 
-    public static Map<String, String> createToken(String email, String requestUri) {
+    public static TokenResponse createToken(String email, String requestUri) {
 
         String accessToken = JWT.create()
                 .withSubject(email)
-                .withExpiresAt(new Date(System.currentTimeMillis() + ACCESS_TOKEN_TIME))
+                .withExpiresAt(new Date(System.currentTimeMillis() + accessTokenDuration))
                 .withIssuer(requestUri)
                 .sign(algorithm);
 
         String refreshToken = JWT.create()
                 .withSubject(email)
-                .withExpiresAt(new Date(System.currentTimeMillis() + REFRESH_TOKEN_TIME))
+                .withExpiresAt(new Date(System.currentTimeMillis() + refreshTokenDuration))
                 .withIssuer(requestUri)
                 .sign(algorithm);
 
-        HashMap<String, String> tokens = new HashMap<>();
-        tokens.put("accessToken", accessToken);
-        tokens.put("refreshToken", refreshToken);
 
-        return tokens;
+        return new TokenResponse(accessToken, refreshToken);
     }
 
-    public static String verifyToken(String token) {
+    public static Map<String, Object> verifyToken(String token) {
         JWTVerifier verifier = JWT.require(algorithm).build();
         DecodedJWT decodedJWT = verifier.verify(token);
-        return decodedJWT.getSubject();
+
+        Map<String, Object> tokenInfo = new HashMap<>();
+        tokenInfo.put("expireDate", decodedJWT.getExpiresAt());
+        tokenInfo.put("subject", decodedJWT.getSubject());
+        return tokenInfo;
     }
+
+
 }
