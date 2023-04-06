593350f693d17fb838729979ab00e5d3abbd02d6
JeonghoPark <pjhpjh0805@naver.com>
2023-04-06 14:20:43 +0900

    feat: Oauth2Controller 기능 구현
    <한것>
    프론트로 부터 AuthorizationCode를 받아 서버의 Jwt를 최종적으로 반환하는
    컨트롤러 입니다.

---
+++ core/src/main/java/com/lastone/core/oauth2/Oauth2Controller.java
@@ -0,0 +1,33 @@
+package com.lastone.core.oauth2;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.lastone.core.dto.response.SuccessResponse;
+import com.lastone.core.security.jwt.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/oauth2/login")
+@RequiredArgsConstructor
+public class Oauth2Controller {
+
+    private final Oauth2ServiceProvider oauth2ServiceProvider;
+
+    @PostMapping("/{registerId}")
+    public ResponseEntity<Object> getToken(HttpServletRequest request, @PathVariable("registerId") String registerId, @RequestBody Map<String,String> authCode) throws JsonProcessingException {
+        Oauth2Service oauth2Service = oauth2ServiceProvider.getOauth2Service(registerId);
+        TokenResponse tokens = oauth2Service.createToken(authCode.get("code"), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(SuccessResponse.builder()
+                        .message("Oauth2 로그인이 완료되었습니다.")
+                        .data(tokens)
+                        .build());
+    }
+}
