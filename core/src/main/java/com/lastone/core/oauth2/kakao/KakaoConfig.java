c6fc541be121b73b37ab7a2dfd9f43bbf84cb335
JeonghoPark <pjhpjh0805@naver.com>
2023-04-06 14:32:20 +0900

    feat: kakao Oauth2 로그인 관련 클래스 작성
    <한것>
    - security에서 oauth2로 패키지 분리를 하였습니다.
    - kakaoOauth2UserInfo 클래스의 성별 반환 값을 남-> 남성 / 여 -> 여성 으로 수정했습니다.
    - 카카오와 통신할 때 필요한 고정 값들을 손쉽게 가져오기 위해 kakaoConfig 클래스를 작성하였습니다.
    - 카카오로 부터 토큰 정보를 받기 위해 KakaoToken 관련 클래스를 작성하였습니다.

---
+++ core/src/main/java/com/lastone/core/oauth2/kakao/KakaoConfig.java
@@ -0,0 +1,31 @@
+package com.lastone.core.oauth2.kakao;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@Setter
+@ToString
+public class KakaoConfig {
+
+    private String clientId;
+    private String clientSecret;
+    private String grantType;
+    private String redirectUrl;
+    private String authorizationUrl;
+    private String tokenUrl;
+    private String userInfoUri;
+
+    public MultiValueMap<String, String> getTokenParams(String authCode) {
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("code", authCode);
+        params.add("grant_type", grantType);
+        params.add("client_id", clientId);
+        params.add("redirect_uri", redirectUrl);
+        params.add("client_secret", clientSecret);
+        return params;
+    }
+}
