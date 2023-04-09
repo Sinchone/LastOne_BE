aba830e73f5937a3dd52a901dce6e625b756aab5
JeonghoPark <pjhpjh0805@naver.com>
2023-04-10 02:39:05 +0900

    fix: JwtProvider 생성 에러 해결
    알고리즘 생성시 secretKey를 인식하지 못하여 빈 생성이 안되는 문제 해결
    @PostConstruct 어노테이션을 활용하여 빈이 생성 된 후에 secretKey를 반영하여
    알고리즘에 값을 넣어주는 방식으로 변경

--- core/src/main/java/com/lastone/core/security/jwt/JwtProvider.java
+++ core/src/main/java/com/lastone/core/security/jwt/JwtProvider.java
@@ -6,7 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
+import javax.annotation.PostConstruct;
 import java.util.*;
 
 @Component
@@ -21,8 +21,12 @@ public class JwtProvider {
     @Value("${token.secretkey}")
     private String secretKey;
 
-    private final Algorithm algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    private Algorithm algorithm;
 
+    @PostConstruct
+    private void setAlgorithm() {
+        this.algorithm = Algorithm.HMAC256(secretKey.getBytes());
+    }
 
     public TokenResponse createToken(String email, String requestUri) {
 
