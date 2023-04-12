56a343d52bc7ca534f66fa15d51d17dae13f5b8d
JeonghoPark <pjhpjh0805@naver.com>
2023-04-12 19:46:38 +0900

    feat: recruitment에 여러 이미지 등록 시 이를 처리할 수 있는 s3 메서드 생성

--- api-server/src/main/java/com/lastone/apiserver/service/s3/S3Service.java
+++ api-server/src/main/java/com/lastone/apiserver/service/s3/S3Service.java
@@ -3,10 +3,13 @@ package com.lastone.apiserver.service.s3;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 public interface S3Service {
 
-    public String upload(MultipartFile multipartFile) throws IOException;
+    List<String> upload(List<MultipartFile> multipartFiles) throws IOException;
 
-    public void delete(String fileName);
+    String upload(MultipartFile multipartFile) throws IOException;
+
+    void delete(String fileName);
 }
