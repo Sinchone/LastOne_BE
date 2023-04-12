83003b64cc1e397437326504ee4e3693a8563628
JeonghoPark <pjhpjh0805@naver.com>
2023-04-12 19:40:08 +0900

    feat: recruitment 관련 repository 생성

---
+++ core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepository.java
@@ -0,0 +1,8 @@
+package com.lastone.core.repository.recruitment;
+
+import com.lastone.core.domain.recruitment.Recruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
+}
---
+++ core/src/main/java/com/lastone/core/repository/recruitment_img/RecruitmentImgRepository.java
@@ -0,0 +1,7 @@
+package com.lastone.core.repository.recruitment_img;
+
+import com.lastone.core.domain.recruitment_img.RecruitmentImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentImgRepository extends JpaRepository<RecruitmentImg, Long> {
+}
