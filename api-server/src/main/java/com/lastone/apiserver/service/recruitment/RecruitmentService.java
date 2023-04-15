aa98d4c234663fec4ba98e7768a29c75e7c78faf
JeonghoPark <pjhpjh0805@naver.com>
2023-04-16 00:53:17 +0900

    feat: 모집글 리스트 관련 ServiceLayer 작성
    <한 것>
    Pageable을 생성하여 repository로부터 모집글 정보가 담긴 Page객체를 반환
    모집글 생성 부분에서 title 누락된 부분 추가

--- api-server/src/main/java/com/lastone/apiserver/service/recruitment/RecruitmentService.java
+++ api-server/src/main/java/com/lastone/apiserver/service/recruitment/RecruitmentService.java
@@ -1,6 +1,9 @@
 package com.lastone.apiserver.service.recruitment;
 
 import com.lastone.core.dto.recruitment.RecruitmentCreateDto;
+import com.lastone.core.dto.recruitment.RecruitmentListDto;
+import com.lastone.core.dto.recruitment.RecruitmentSearchCondition;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -9,4 +12,5 @@ import java.util.List;
 public interface RecruitmentService {
 
     void createRecruitment(Long memberId, RecruitmentCreateDto recruitmentCreateDto, List<MultipartFile> imgFiles) throws IOException;
+    Page<RecruitmentListDto> getList(RecruitmentSearchCondition searchCondition);
 }
