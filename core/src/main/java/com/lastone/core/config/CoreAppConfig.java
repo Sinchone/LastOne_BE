40af7c707b09422c72903b036f6c8f6c86a96079
JeonghoPark <pjhpjh0805@naver.com>
2023-04-15 23:46:25 +0900

    feat: ObjectMapper 빈관련 설정 변경
    - LocalDateTime 직렬화 및 역직렬화를 위한 모듈을 생성 인자로 추가

--- core/src/main/java/com/lastone/core/config/CoreAppConfig.java
+++ core/src/main/java/com/lastone/core/config/CoreAppConfig.java
@@ -1,6 +1,7 @@
 package com.lastone.core.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.lastone.core.oauth2.kakao.KakaoConfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,7 +18,8 @@ public class CoreAppConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper();
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.registerModule(new JavaTimeModule());
     }
 
     @Bean
