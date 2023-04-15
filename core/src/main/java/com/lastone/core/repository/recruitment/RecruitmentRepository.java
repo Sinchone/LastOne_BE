e7bd51a20e0e5a3f5c49e00c0b81c1ecabd6239c
JeonghoPark <pjhpjh0805@naver.com>
2023-04-15 23:52:35 +0900

    feat: querydsl 사용을 위한 customRepository 생성 및 상속 설정
    - LocalDateTime 직렬화 및 역직렬화를 위한 모듈을 생성 인자로 추가

--- core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepository.java
+++ core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepository.java
@@ -3,6 +3,6 @@ package com.lastone.core.repository.recruitment;
 import com.lastone.core.domain.recruitment.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>, RecruitmentRepositoryCustom {
 
 }
---
+++ core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.lastone.core.repository.recruitment;
+
+import com.lastone.core.dto.recruitment.RecruitmentListDto;
+import com.lastone.core.dto.recruitment.RecruitmentSearchCondition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface RecruitmentRepositoryCustom {
+
+    Page<RecruitmentListDto> getListDto(Pageable pageable, RecruitmentSearchCondition searchCondition);
+}
