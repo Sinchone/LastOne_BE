6240aaca7358e24e83ac1990620c6c860280b42e
JeonghoPark <pjhpjh0805@naver.com>
2023-04-16 00:50:42 +0900

    fix: recruitment 상태값을 위한 enum 생성

--- core/src/main/java/com/lastone/core/domain/recruitment/RecruitmentStatus.java
+++ core/src/main/java/com/lastone/core/domain/recruitment/RecruitmentStatus.java
@@ -1,10 +1,13 @@
 package com.lastone.core.domain.recruitment;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RecruitmentStatus {
     RECRUITING("모집중"),
     COMPLETE("모집완료"),
     EXPIRATION("기간만료");
 
+    @JsonValue
     private final String text;
 
     RecruitmentStatus(String text) {
