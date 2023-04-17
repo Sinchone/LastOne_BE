ca540cb0954af227093f65df8a65b86d8dd965a1
beetrbgus <devbeetrb0214@gmail.com>
2023-04-17 20:08:26 +0900

    feature - status Code 값 변경
    # 한 것
    - 상태값 제거

--- core/src/main/java/com/lastone/core/common/response/SuccessCode.java
+++ core/src/main/java/com/lastone/core/common/response/SuccessCode.java
@@ -4,15 +4,13 @@ import lombok.Getter;
 
 @Getter
 public enum SuccessCode {
-    CREATED_CHAT_ROOM(201, "채팅방이 생성되었습니다."),
-    DELETED_CHAT_ROOM(200, "채팅방이 삭제되었습니다."),
-    GET_CHAT_ROOM_LIST(200, "채팅방 목록 조회에 성공하였습니다.");
+    CREATED_CHAT_ROOM("채팅방이 생성되었습니다."),
+    DELETED_CHAT_ROOM("채팅방이 삭제되었습니다."),
+    GET_CHAT_ROOM_LIST("채팅방 목록 조회에 성공하였습니다.");
 
-    private int status;
     private String message;
 
-    SuccessCode(int status, String message) {
-        this.status = status;
+    SuccessCode(String message) {
         this.message = message;
     }
 }
