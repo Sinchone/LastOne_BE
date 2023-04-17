16dca60c738da5d2fc91d7e02f58f7bf2a602222
beetrbgus <devbeetrb0214@gmail.com>
2023-04-18 04:16:00 +0900

    feature - Chatting 노트북용 푸쉬
    # 한 것
    - 헬스 체크를 위한 컨트롤러 생성

---
+++ chatting-server/src/main/java/com/lastone/chat/controller/HealthCheckController.java
@@ -0,0 +1,12 @@
+package com.lastone.chat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+    @GetMapping
+    public void getHealthCheck() {}
+}
