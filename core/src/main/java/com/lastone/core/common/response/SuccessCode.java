8f4126e4c37bc9f294acea68da0255c400b92922
JeonghoPark <pjhpjh0805@naver.com>
2023-04-17 17:03:01 +0900

    feat: SuccessCode 모집글, 마이페이지, 토큰 관련 추가 작성

--- core/src/main/java/com/lastone/core/common/response/SuccessCode.java
+++ core/src/main/java/com/lastone/core/common/response/SuccessCode.java
@@ -6,7 +6,24 @@ import lombok.Getter;
 public enum SuccessCode {
     CREATED_CHAT_ROOM(201, "채팅방이 생성되었습니다."),
     DELETED_CHAT_ROOM(200, "채팅방이 삭제되었습니다."),
-    GET_CHAT_ROOM_LIST(200, "채팅방 목록 조회에 성공하였습니다.");
+    GET_CHAT_ROOM_LIST(200, "채팅방 목록 조회에 성공하였습니다."),
+
+    /* My Page */
+    INQUIRE_MYPAGE(200, "마이페이지 정보 조회에 성공하였습니다."),
+    UPDATE_MYPAGE(200, "마이페이지 수정 작업에 성공하였습니다."),
+
+    /* TOKEN */
+    OAUTH2_LOGIN(201, "Oauth2 로그인이 완료되었습니다."),
+    TOKEN_LOGOUT(200, "로그아웃을 완료하였습니다."),
+    TOKEN_REFRESH(201, "토큰 재발급을 완료하였습니다."),
+
+    /* 모집글 */
+    RECRUITMENT_LIST(200, "모집글 리스트 조회에 성공하였습니다."),
+    RECRUITMENT_DETAIL(200, "모집글 상세 정보 조회에 성공하였습니다."),
+
+    RECRUITMENT_CREATE(201, "모집글 작성이 완료되었습니다."),
+
+    ;
 
     private int status;
     private String message;
