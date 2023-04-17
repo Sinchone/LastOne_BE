ba9b07c70523b84862bdb25697d0397f115b9e40
JeonghoPark <pjhpjh0805@naver.com>
2023-04-17 16:14:43 +0900

    feat: 모집글 dto 유효성 검증 커스텀 validator 생성

---
+++ core/src/main/java/com/lastone/core/util/validator/recruitment/RecruitmentTimeValidator.java
@@ -0,0 +1,33 @@
+package com.lastone.core.util.validator.recruitment;
+
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
+import java.util.List;
+
+public class RecruitmentTimeValidator implements ConstraintValidator<RecruitmentTime, String> {
+
+    private static final int HOUR = 0;
+    private static final int MINUTE = 1;
+    private static final int TIME_COUNT = 2;
+    private static final String MINUTE_DEFAULT_VALUE = "00";
+    private static final List<Integer> HOURS_TYPE = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        String[] times = value.split(":");
+        if (times.length != TIME_COUNT) {
+            return false;
+        }
+        String hour = times[HOUR];
+        String minute = times[MINUTE];
+        if (!HOURS_TYPE.contains(Integer.parseInt(hour))) {
+            return false;
+        }
+        if (!minute.equals(MINUTE_DEFAULT_VALUE)) {
+            return false;
+        }
+        return true;
+    }
+}
