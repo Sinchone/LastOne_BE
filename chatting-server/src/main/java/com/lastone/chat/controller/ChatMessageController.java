1c204636189e1e4bfbdc93de16ff2bbb35248e08
beetrbgus <devbeetrb0214@gmail.com>
2023-04-18 17:36:50 +0900

    feature - 채팅 # 한 것 - 배포시 테스트 할 로그인 처리 추가 및 유효성 검사 추가
    - 백엔드 로컬에서 테스트 하기 위한 메시지 보내기 테스트 메서드 추가

--- chatting-server/src/main/java/com/lastone/chat/controller/ChatMessageController.java
+++ chatting-server/src/main/java/com/lastone/chat/controller/ChatMessageController.java
@@ -1,13 +1,17 @@
 package com.lastone.chat.controller;
 
 import com.lastone.chat.dto.ChatMessageResDto;
+import com.lastone.chat.exception.ChatException;
 import com.lastone.chat.service.ChatMessageService;
+import com.lastone.core.common.response.ErrorCode;
 import com.lastone.core.dto.message.ChatMessageReqDto;
+import com.lastone.core.security.principal.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
@@ -16,16 +20,20 @@ import org.springframework.stereotype.Controller;
 public class ChatMessageController {
     private final SimpMessagingTemplate template;
     private final ChatMessageService messageService;
-    /**
-     * Todo - API 연동이 끝나면 통합테스트를 위해 로그인 로직 붙이기
-     * @AuthenticationPrincipal UserDetailsImpl userDetails,
-     * if(userDetails.getId() != chatMessage.getSenderId()
-     *         || userDetails.getId() != chatMessage.getReceiverId()) {
-     *             throw new ChatException(ErrorCode.UNAUTHORIZED);
-     *         }
-     */
+
     @MessageMapping("chat/message/{chatRoomId}")
-    public void sendMessage(@DestinationVariable String chatRoomId, ChatMessageReqDto chatMessage) {
+    public void sendMessage(@AuthenticationPrincipal UserDetailsImpl userDetails, @DestinationVariable String chatRoomId, ChatMessageReqDto chatMessage) {
+        ChatMessageResDto message = messageService.createMessage(chatRoomId, chatMessage);
+        if(userDetails.getId() != chatMessage.getSenderId()
+                || userDetails.getId() != chatMessage.getReceiverId()) {
+            throw new ChatException(ErrorCode.UNAUTHORIZED);
+        }
+        log.info("chatMessage  : " + chatMessage.getContent());
+        template.convertAndSend("/sub/chat/room/" + chatRoomId, message);
+    }
+
+    @MessageMapping("/chat/message/test/{chatRoomId}")
+    public void testSendMessage(@DestinationVariable String chatRoomId, ChatMessageReqDto chatMessage) {
         ChatMessageResDto message = messageService.createMessage(chatRoomId, chatMessage);
         log.info("chatMessage  : " + chatMessage.getContent());
         template.convertAndSend("/sub/chat/room/" + chatRoomId, message);
