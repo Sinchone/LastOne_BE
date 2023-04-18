3ea40505c766e755c2c2db114cee70ec9e305a33
beetrbgus <devbeetrb0214@gmail.com>
2023-04-18 22:24:23 +0900

    feature - Message 보낼시에는 검증하지 않음 # 한 것 - 채팅방 상세까지 검증을 하기 때문에 메시지를 보낼 때에는 검증하지 않게 변경 - ResDto 반환 다른 유저 정보 수정 - Test시 파라미터로 회원 정보를 설정할 수 있게   - chat.html도 동일하게 파라미터 입력한 것으로 회원정보 수정할 수 있게

--- chatting-server/src/main/java/com/lastone/chat/controller/ChatMessageController.java
+++ chatting-server/src/main/java/com/lastone/chat/controller/ChatMessageController.java
@@ -1,17 +1,13 @@
 package com.lastone.chat.controller;
 
 import com.lastone.chat.dto.ChatMessageResDto;
-import com.lastone.chat.exception.ChatException;
 import com.lastone.chat.service.ChatMessageService;
-import com.lastone.core.common.response.ErrorCode;
 import com.lastone.core.dto.message.ChatMessageReqDto;
-import com.lastone.core.security.principal.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
 @Slf4j
@@ -22,13 +18,8 @@ public class ChatMessageController {
     private final ChatMessageService messageService;
 
     @MessageMapping("chat/message/{chatRoomId}")
-    public void sendMessage(@AuthenticationPrincipal UserDetailsImpl userDetails, @DestinationVariable String chatRoomId, ChatMessageReqDto chatMessage) {
+    public void sendMessage(ChatMessageReqDto chatMessage, @DestinationVariable String chatRoomId) {
         ChatMessageResDto message = messageService.createMessage(chatRoomId, chatMessage);
-        if(userDetails.getId() != chatMessage.getSenderId()
-                || userDetails.getId() != chatMessage.getReceiverId()) {
-            throw new ChatException(ErrorCode.UNAUTHORIZED);
-        }
-        log.info("chatMessage  : " + chatMessage.getContent());
         template.convertAndSend("/sub/chat/room/" + chatRoomId, message);
     }
 
