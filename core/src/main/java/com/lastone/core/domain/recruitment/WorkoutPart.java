ffad78e2faa01e1aa9872fb336f71813cb1dff9f
JeonghoPark <pjhpjh0805@naver.com>
2023-04-20 03:08:35 +0900

    feat: 메인 페이지 운동 부위 관련 imgUrl 추가

--- core/src/main/java/com/lastone/core/domain/recruitment/WorkoutPart.java
+++ core/src/main/java/com/lastone/core/domain/recruitment/WorkoutPart.java
@@ -4,20 +4,22 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum WorkoutPart {
-    ALL("전신", "all.jpg"),
-    CHEST("가슴", "chest.jpg"),
-    BACK("등", "back.jpg"),
-    SHOULDER("어깨", "shoulder.jpg"),
-    LOWER("하체", "lower.jpg"),
-    CORE("코어", "core.jpg");
+    ALL("전신", "all-main.jpg", "all-list.jpg"),
+    CHEST("가슴", "chest-main.jpg", "chest-list.jpg"),
+    BACK("등", "back-main.jpg", "back-list.jpg"),
+    SHOULDER("어깨", "shoulder-main.jpg", "shoulder-list.jpg"),
+    LOWER("하체", "lower-main.jpg", "lower-list.jpg"),
+    CORE("코어", "core-main.jpg", "core-list.jpg");
 
     private final String text;
 
-    private final String defaultImgUrl;
+    private final String mainDefaultImgUrl;
+    private final String listDefaultImgUrl;
 
-    WorkoutPart(String text, String defaultImgUrl) {
+    WorkoutPart(String text, String mainDefaultImgUrl, String listDefaultImgUrl) {
         this.text = text;
-        this.defaultImgUrl = defaultImgUrl;
+        this.mainDefaultImgUrl = mainDefaultImgUrl;
+        this.listDefaultImgUrl = listDefaultImgUrl;
     }
 
     @JsonCreator
@@ -35,7 +37,11 @@ public enum WorkoutPart {
         return text;
     }
 
-    public String getDefaultImgUrl() {
-        return defaultImgUrl;
+    public String getListDefaultImgUrl() {
+        return listDefaultImgUrl;
+    }
+
+    public String getMainDefaultImgUrl() {
+        return mainDefaultImgUrl;
     }
 }
