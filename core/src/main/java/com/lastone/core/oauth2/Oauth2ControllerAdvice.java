cd59d39476679ced5a6f00288028359b12ce3961
JeonghoPark <pjhpjh0805@naver.com>
2023-04-19 17:21:15 +0900

    feat: oauth2 컨트롤러 및 컨트롤러 어드바이스 로직 수정

--- core/src/main/java/com/lastone/core/oauth2/Oauth2ControllerAdvice.java
+++ core/src/main/java/com/lastone/core/oauth2/Oauth2ControllerAdvice.java
@@ -1,22 +1,40 @@
 package com.lastone.core.oauth2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lastone.core.common.response.CommonResponse;
 import com.lastone.core.common.response.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Objects;
 
-@RestControllerAdvice
+
+@Slf4j
+@RestControllerAdvice(assignableTypes = Oauth2Controller.class)
+@RequiredArgsConstructor
 public class Oauth2ControllerAdvice extends ResponseEntityExceptionHandler {
+    private static final String ALREADY_USED_CODE = "KOE320";
 
     @ExceptionHandler(Oauth2Exception.class)
-    public ResponseEntity<Object> globalExceptionHandler(Oauth2Exception e) {
+    public ResponseEntity<CommonResponse> globalExceptionHandler(Oauth2Exception e) {
         return handleExceptionInternal(e);
     }
 
-    private ResponseEntity<Object> handleExceptionInternal(Oauth2Exception e) {
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponse> globalExceptionHandler(Exception e) {
+        log.info(e.getMessage());
+        if(Objects.requireNonNull(e.getMessage()).contains(ALREADY_USED_CODE)) {
+            return handleExceptionInternal(new AlreadyUsedCodeException());
+        }
+        return handleExceptionInternal(new Oauth2DefaultException());
+    }
+
+    private ResponseEntity<CommonResponse> handleExceptionInternal(Oauth2Exception e) {
         ErrorCode errorCode = e.getErrorCode();
         return ResponseEntity.status(errorCode.getStatus())
                 .body(CommonResponse.fail(errorCode));
