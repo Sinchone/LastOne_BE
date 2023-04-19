fd385dbc56b63a94fbe7d4d01a95fe199c25428d
JeonghoPark <pjhpjh0805@naver.com>
2023-04-19 17:38:22 +0900

    fix: oauth2 인증에 필요한 빈 CoreAppConfig 에서 api AppConfig로 이동

--- core/src/main/java/com/lastone/core/config/CoreAppConfig.java
+++ core/src/main/java/com/lastone/core/config/CoreAppConfig.java
@@ -2,29 +2,15 @@ package com.lastone.core.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.lastone.core.oauth2.kakao.KakaoConfig;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class CoreAppConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
-
-    @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.registerModule(new JavaTimeModule());
-    }
-
-    @Bean
-    @ConfigurationProperties(prefix = "oauth2.kakao")
-    public KakaoConfig kakaoConfig() {
-        return new KakaoConfig();
     }
 }
