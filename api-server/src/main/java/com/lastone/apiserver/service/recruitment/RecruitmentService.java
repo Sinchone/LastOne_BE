cb56070245f41f47eb043d1f329c3bb21d7f2d87
JeonghoPark <pjhpjh0805@naver.com>
2023-04-19 15:40:02 +0900

    fix: 무한 스크롤 방식을 위한 Page객체 반환에서 Slice객체 반환으로 반환타입 변경

--- api-server/src/main/java/com/lastone/apiserver/service/recruitment/RecruitmentService.java
+++ api-server/src/main/java/com/lastone/apiserver/service/recruitment/RecruitmentService.java
@@ -4,16 +4,15 @@ import com.lastone.core.dto.recruitment.RecruitmentRequestDto;
 import com.lastone.core.dto.recruitment.RecruitmentDetailDto;
 import com.lastone.core.dto.recruitment.RecruitmentListDto;
 import com.lastone.core.dto.recruitment.RecruitmentSearchCondition;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Slice;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
 
 public interface RecruitmentService {
 
     void createRecruitment(Long memberId, RecruitmentRequestDto recruitmentCreateDto, List<MultipartFile> imgFiles) throws IOException;
-    Page<RecruitmentListDto> getList(RecruitmentSearchCondition searchCondition);
+    Slice<RecruitmentListDto> getList(RecruitmentSearchCondition searchCondition);
     RecruitmentDetailDto getDetail(Long recruitmentId);
     void updateRecruitment(Long recruitmentId, Long memberId, RecruitmentRequestDto recruitment, List<MultipartFile> imgFiles) throws IOException;
     void deleteRecruitment(Long recruitmentId, Long id);
