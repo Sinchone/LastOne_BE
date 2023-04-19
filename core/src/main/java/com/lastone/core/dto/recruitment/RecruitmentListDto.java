6001d64d53fe2dcd5401c4fd1d3274282875629a
JeonghoPark <pjhpjh0805@naver.com>
2023-04-20 02:35:58 +0900

    feat: 모집글 리스트 가져올 시 등록된 이미지 및 일러스트 분기처리 구현

--- core/src/main/java/com/lastone/core/dto/recruitment/RecruitmentListDto.java
+++ core/src/main/java/com/lastone/core/dto/recruitment/RecruitmentListDto.java
@@ -4,15 +4,23 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.lastone.core.domain.recruitment.PreferGender;
+import com.lastone.core.domain.recruitment.Recruitment;
 import com.lastone.core.domain.recruitment.RecruitmentStatus;
 import com.lastone.core.domain.recruitment.WorkoutPart;
-import com.querydsl.core.annotations.QueryProjection;
-import lombok.*;
-
+import com.lastone.core.domain.recruitment_img.RecruitmentImg;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
+@Builder
+@AllArgsConstructor
 public class RecruitmentListDto {
 
     private Long id;
@@ -31,15 +39,33 @@ public class RecruitmentListDto {
 
     private WorkoutPart workoutPart;
 
+    private String imgUrl;
 
-    @QueryProjection
-    public RecruitmentListDto(Long id, String title, String gym, LocalDateTime startedAt, RecruitmentStatus status, PreferGender preferGender, WorkoutPart workoutPart) {
-        this.id = id;
-        this.title = title;
-        this.gym = gym;
-        this.startedAt = startedAt;
-        this.status = status;
-        this.preferGender = preferGender;
-        this.workoutPart = workoutPart;
+
+    public static List<RecruitmentListDto> toDto(List<Recruitment> recruitments) {
+        List<RecruitmentListDto> recruitmentListDtos = new ArrayList<>();
+        for (Recruitment recruitment : recruitments) {
+
+            RecruitmentListDto recruitmentListDto = RecruitmentListDto.builder()
+                    .id(recruitment.getId())
+                    .gym(recruitment.getGym().getName())
+                    .status(recruitment.getRecruitmentStatus())
+                    .title(recruitment.getTitle())
+                    .preferGender(recruitment.getPreferGender())
+                    .startedAt(recruitment.getStartedAt())
+                    .workoutPart(recruitment.getWorkoutPart())
+                    .imgUrl(toImgUrl(recruitment))
+                    .build();
+            recruitmentListDtos.add(recruitmentListDto);
+        }
+        return recruitmentListDtos;
+    }
+
+    private static String toImgUrl(Recruitment recruitment) {
+        List<RecruitmentImg> recruitmentImgs = recruitment.getRecruitmentImgs();
+        if (recruitmentImgs.isEmpty()) {
+            return recruitment.getWorkoutPart().getDefaultImgUrl();
+        }
+        return recruitmentImgs.get(0).getImgUrl();
     }
 }
--- core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepositoryImpl.java
+++ core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepositoryImpl.java
@@ -1,10 +1,10 @@
 package com.lastone.core.repository.recruitment;
 
 import com.lastone.core.domain.recruitment.PreferGender;
+import com.lastone.core.domain.recruitment.Recruitment;
 import com.lastone.core.domain.recruitment.RecruitmentStatus;
 import com.lastone.core.domain.recruitment.WorkoutPart;
 import com.lastone.core.dto.recruitment.QRecruitmentDetailDto;
-import com.lastone.core.dto.recruitment.QRecruitmentListDto;
 import com.lastone.core.dto.recruitment.RecruitmentDetailDto;
 import com.lastone.core.dto.recruitment.RecruitmentListDto;
 import com.lastone.core.dto.recruitment.RecruitmentSearchCondition;
@@ -15,6 +15,8 @@ import org.springframework.data.domain.*;
 import org.springframework.util.ObjectUtils;
 import java.time.LocalDateTime;
 import java.util.List;
+import static com.lastone.core.domain.gym.QGym.gym;
+import static com.lastone.core.domain.member.QMember.member;
 import static com.lastone.core.domain.recruitment.QRecruitment.recruitment;
 import static com.lastone.core.domain.recruitment_img.QRecruitmentImg.recruitmentImg;
 
@@ -64,17 +66,11 @@ public class RecruitmentRepositoryImpl implements RecruitmentRepositoryCustom{
 
         Pageable pageable = PageRequest.of(DEFAULT_OFFSET, DEFAULT_SIZE);
 
-        List<RecruitmentListDto> content = queryFactory
-                .select(new QRecruitmentListDto(
-                        recruitment.id,
-                        recruitment.title,
-                        recruitment.gym.name,
-                        recruitment.startedAt,
-                        recruitment.recruitmentStatus,
-                        recruitment.preferGender,
-                        recruitment.workoutPart
-                ))
-                .from(recruitment)
+        List<Recruitment> content = queryFactory
+                .selectFrom(recruitment)
+                .leftJoin(recruitment.member, member).fetchJoin()
+                .leftJoin(recruitment.gym, gym).fetchJoin()
+                .leftJoin(recruitment.recruitmentImgs, recruitmentImg).fetchJoin()
                 .where(
                         eqWorkoutPart(searchCondition.getWorkoutPart()),
                         eqPreferGender(searchCondition.getPreferGender()),
@@ -145,12 +141,13 @@ public class RecruitmentRepositoryImpl implements RecruitmentRepositoryCustom{
         return recruitment.id.lt(lastId);
     }
 
-    private Slice<RecruitmentListDto> checkLastPage(Pageable pageable, List<RecruitmentListDto> content) {
+    private Slice<RecruitmentListDto> checkLastPage(Pageable pageable, List<Recruitment> recruitments) {
         boolean hasNext = false;
-        if (content.size() > DEFAULT_SIZE) {
+        if (recruitments.size() > DEFAULT_SIZE) {
             hasNext = true;
-            content.remove(DEFAULT_SIZE);
+            recruitments.remove(DEFAULT_SIZE);
         }
+        List<RecruitmentListDto> content = RecruitmentListDto.toDto(recruitments);
         return new SliceImpl<>(content, pageable, hasNext);
     }
 }
