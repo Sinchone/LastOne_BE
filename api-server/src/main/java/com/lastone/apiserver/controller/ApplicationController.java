da4f9c81693e94708daf6a329bf943e359a1540c
JeonghoPark <pjhpjh0805@naver.com>
2023-04-22 02:32:32 +0900

    feat: 받은 신청 리스트 조회 관련 컨트롤러 작성

--- api-server/src/main/java/com/lastone/apiserver/controller/ApplicationController.java
+++ api-server/src/main/java/com/lastone/apiserver/controller/ApplicationController.java
@@ -3,6 +3,7 @@ package com.lastone.apiserver.controller;
 import com.lastone.apiserver.service.application.ApplicationService;
 import com.lastone.core.common.response.CommonResponse;
 import com.lastone.core.common.response.SuccessCode;
+import com.lastone.core.dto.applicaation.ApplicationReceivedDto;
 import com.lastone.core.security.principal.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,8 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -21,11 +22,12 @@ import java.util.HashMap;
 public class ApplicationController {
     private final ApplicationService applicationService;
 
-//    @GetMapping("/received")
-//    @PreAuthorize("isAuthenticated()")
-//    public ResponseEntity<CommonResponse> getReceivedApplication(@AuthenticationPrincipal UserDetailsImpl userDetails) {
-//
-//    }
+    @GetMapping("/received")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<CommonResponse> getReceivedApplication(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<ApplicationReceivedDto> content = applicationService.getReceivedListByMemberId(userDetails.getId());
+        return ResponseEntity.ok().body(CommonResponse.success(SuccessCode.APPLICATION_RECEIVED_LIST, content));
+    }
 
 
     @PostMapping
