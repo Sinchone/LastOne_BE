878698cebd49a3c15cc0f39d3f018ef8cecbdef5
JeonghoPark <pjhpjh0805@naver.com>
2023-04-21 22:22:35 +0900

    feat: ApplymentRepository 작성 및 신청 생성을 위한 RecruitmentRepository 메서드 추가

--- core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepository.java
+++ core/src/main/java/com/lastone/core/repository/recruitment/RecruitmentRepository.java
@@ -2,7 +2,12 @@ package com.lastone.core.repository.recruitment;
 
 import com.lastone.core.domain.recruitment.Recruitment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long>, RecruitmentRepositoryCustom {
 
+    @Query("select r from Recruitment r where r.id = :id and r.isDeleted = false")
+    Optional<Recruitment> findByIdAndDeletedIsFalse(Long id);
 }
