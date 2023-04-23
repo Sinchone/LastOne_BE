7c83dc46e8020462da688eaf587d505d4689b25d
JeonghoPark <pjhpjh0805@naver.com>
2023-04-23 17:30:57 +0900

    fix: security debug 설정 해제

--- core/src/main/java/com/lastone/core/config/SecurityConfig.java
+++ core/src/main/java/com/lastone/core/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity(debug = false)
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
