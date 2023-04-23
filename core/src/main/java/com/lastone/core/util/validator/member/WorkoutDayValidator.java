6a52d7cf8142f94d3e1607c4f70c5cc919457e95
JeonghoPark <pjhpjh0805@naver.com>
2023-04-23 17:14:11 +0900

    fix: 입력 요일 정보가 배열로 변경됨에 따라 검증 유효성 검증 로직 변경

--- core/src/main/java/com/lastone/core/util/validator/member/WorkoutDayValidator.java
+++ core/src/main/java/com/lastone/core/util/validator/member/WorkoutDayValidator.java
@@ -1,25 +1,18 @@
 package com.lastone.core.util.validator.member;
 
+import com.lastone.core.domain.member.Day;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
 import java.util.List;
 
-public class WorkoutDayValidator implements ConstraintValidator<WorkoutDay, String> {
-
-    private static final List<String> VALID_DAYS = Arrays.asList("월", "화", "수", "목", "금", "토", "일");
+public class WorkoutDayValidator implements ConstraintValidator<WorkoutDay, List<String>> {
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null) {
-            return true; // null values are considered valid
+    public boolean isValid(List<String> days, ConstraintValidatorContext context) {
+        if (days.isEmpty()) {
+            return true;
         }
-        String[] days = value.split(",");
-        for (String day : days) {
-            if (!VALID_DAYS.contains(day.trim())) {
-                return false;
-            }
-        }
-        return true;
+        return Day.isValidType(days) && Day.isDuplicatedDaysExist(days);
     }
 }
