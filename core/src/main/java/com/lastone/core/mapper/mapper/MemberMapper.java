cbac9cd17ae13a93b7fa8b2217dccc44c8ee34a0
JeonghoPark <pjhpjh0805@naver.com>
2023-04-23 17:00:38 +0900

    fix: 요일 정보 프론트 반환 형식 String -> List로 수정됨에 따라 Mapper 로직 수정

--- core/src/main/java/com/lastone/core/mapper/mapper/MemberMapper.java
+++ core/src/main/java/com/lastone/core/mapper/mapper/MemberMapper.java
@@ -1,9 +1,12 @@
 package com.lastone.core.mapper.mapper;
 
+import com.lastone.core.domain.member.Day;
 import com.lastone.core.domain.member.Member;
 import com.lastone.core.dto.member.MemberDto;
 import org.mapstruct.Mapper;
-
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -12,9 +15,24 @@ import java.util.List;
 public interface MemberMapper extends GenericMapper<MemberDto, Member> {
 
 
+    @Mapping(target = "workoutDay", source = "workoutDay", qualifiedByName = "toWorkoutDayAsList")
     @Override
     MemberDto toDto(Member member);
 
+    @Mapping(target = "workoutDay", source = "workoutDay", qualifiedByName = "toWorkoutDayListAsString")
+    @Override
+    Member toEntity(MemberDto dto);
+
     @Override
     List<MemberDto> toDto(List<Member> e);
+
+    @Named("toWorkoutDayAsList")
+    default List<String> toWorkoutDaysList(String workoutDay) {
+        return Arrays.asList(workoutDay.split(" "));
+    }
+
+    @Named("toWorkoutDayListAsString")
+    default String toWorkoutDayListAsString(List<String> workoutDays) {
+        return Day.sortListToString(workoutDays);
+    }
 }
