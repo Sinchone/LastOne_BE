fcc67b1fd0d3c90ac262d377f6fa873beac0d714
JeonghoPark <pjhpjh0805@naver.com>
2023-04-27 19:41:16 +0900

    feat: 오늘의 약속 api 응답 dto 작성

---
+++ core/src/main/java/com/lastone/core/dto/partner/TodayPartnerDto.java
@@ -0,0 +1,29 @@
+package com.lastone.core.dto.partner;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.lastone.core.domain.recruitment.WorkoutPart;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class TodayPartnerDto {
+
+    private Long recruitmentId;
+
+    private String partnerName;
+
+    private WorkoutPart workoutPart;
+
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd 'T' HH:mm")
+    private LocalDateTime startedAt;
+
+    private String gym;
+}
