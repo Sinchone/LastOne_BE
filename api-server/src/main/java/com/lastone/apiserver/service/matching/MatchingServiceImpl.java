bf3dfda7741629f9f380dc10ed8cb280dae64b5b
JeonghoPark <pjhpjh0805@naver.com>
2023-05-03 16:08:12 +0900

    feat: 매칭 서비스 관련 권한 및 중복 검증 로직 추가

--- api-server/src/main/java/com/lastone/apiserver/service/matching/MatchingServiceImpl.java
+++ api-server/src/main/java/com/lastone/apiserver/service/matching/MatchingServiceImpl.java
@@ -4,15 +4,20 @@ import com.lastone.apiserver.exception.application.ApplicationNotFoundException;
 import com.lastone.apiserver.exception.application.ApplicationStatusInCorrectException;
 import com.lastone.apiserver.exception.recruitment.IncorrectWriterException;
 import com.lastone.apiserver.exception.recruitment.RecruitmentNotFoundException;
+import com.lastone.apiserver.exception.recruitment.RecruitmentNotIncludeApplicationException;
+import com.lastone.apiserver.exception.recruitment.RecruitmentStatusException;
 import com.lastone.core.common.response.ErrorCode;
 import com.lastone.core.domain.application.Application;
 import com.lastone.core.domain.application.ApplicationStatus;
 import com.lastone.core.domain.recruitment.Recruitment;
+import com.lastone.core.domain.recruitment.RecruitmentStatus;
 import com.lastone.core.repository.application.ApplicationRepository;
 import com.lastone.core.repository.recruitment.RecruitmentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -24,7 +29,12 @@ public class MatchingServiceImpl implements MatchingService{
     private static final int RECRUITMENT_UPDATED_COUNT = 1;
 
     @Override
-    public void completeMatching(Long recruitmentId, Long applicationId) {
+    public void completeMatching(Long recruitmentId, Long applicationId, Long requesterId) {
+        Recruitment recruitment = recruitmentRepository.findById(recruitmentId).orElseThrow(RecruitmentNotFoundException::new);
+        validateRequesterAuthorization(recruitment.getMember().getId(), requesterId);
+        validateApplicationId(recruitment.getApplications(), applicationId);
+        validateRecruitmentStatus(recruitment);
+
         int updateCount = recruitmentRepository.updateStatusToComplete(recruitmentId);
         if (updateCount != RECRUITMENT_UPDATED_COUNT) {
             throw new RecruitmentNotFoundException();
@@ -33,13 +43,29 @@ public class MatchingServiceImpl implements MatchingService{
     }
 
     @Override
-    public void cancelMatching(Long recruitmentId, Long applicationId, Long requestMemberId) {
+    public void cancelMatching(Long recruitmentId, Long applicationId, Long requesterId) {
         Recruitment recruitment = recruitmentRepository.findById(recruitmentId).orElseThrow(RecruitmentNotFoundException::new);
-        validateDeleteAuthorization(recruitment.getMember().getId(), requestMemberId);
+        validateRequesterAuthorization(recruitment.getMember().getId(), requesterId);
+        validateApplicationId(recruitment.getApplications(), applicationId);
         recruitment.cancelMatching();
         Application application = applicationRepository.findById(applicationId).orElseThrow(ApplicationNotFoundException::new);
         validateApplicationStatus(application);
         application.fail();
+    }
+
+    private void validateRecruitmentStatus(Recruitment recruitment) {
+        if (recruitment.getRecruitmentStatus().equals(RecruitmentStatus.RECRUITING)){
+            return;
+        }
+        throw new RecruitmentStatusException(ErrorCode.IMPOSSIBLE_TO_MATCH_STATUS);
+    }
+
+    /* 해당 모집글에 속하는 신청 Id가 아닐 경우 예외 처리 */
+    private void validateApplicationId(List<Application> applications, Long applicationId) {
+        List<Long> applicationIds = applications.stream().map(Application::getId).collect(Collectors.toList());
+        if (!applicationIds.contains(applicationId)) {
+            throw new RecruitmentNotIncludeApplicationException();
+        }
     }
 
     private void validateApplicationStatus(Application application) {
@@ -48,8 +74,8 @@ public class MatchingServiceImpl implements MatchingService{
         }
     }
 
-    private void validateDeleteAuthorization(Long writerId, Long requestMemberId) {
-        if (!requestMemberId.equals(writerId)) {
+    private void validateRequesterAuthorization(Long writerId, Long requesterId) {
+        if (!requesterId.equals(writerId)) {
             throw new IncorrectWriterException();
         }
     }
