33e53af139b807407bf19a5c3f31d038b36564e0
JeonghoPark <pjhpjh0805@naver.com>
2023-05-05 00:53:57 +0900

    fix: 알림 리스트 관련 컨트롤러 메서드 작성

---
+++ api-server/src/main/java/com/lastone/apiserver/controller/NotificationController.java
@@ -0,0 +1,35 @@
+package com.lastone.apiserver.controller;
+
+import com.lastone.apiserver.service.notification.NotificationService;
+import com.lastone.core.common.response.CommonResponse;
+import com.lastone.core.common.response.SuccessCode;
+import com.lastone.core.dto.notification.NotificationCheckBoxCondition;
+import com.lastone.core.dto.notification.NotificationResponseDto;
+import com.lastone.core.security.principal.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/notification")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping
+    public ResponseEntity<CommonResponse> getNotificationList(NotificationCheckBoxCondition checkBoxCondition,
+                                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<NotificationResponseDto> notificationResponseDtoList = notificationService.getList(checkBoxCondition, userDetails.getId());
+        return ResponseEntity.ok().body(CommonResponse.success(SuccessCode.NOTIFICATION_LIST,notificationResponseDtoList));
+    }
+}
