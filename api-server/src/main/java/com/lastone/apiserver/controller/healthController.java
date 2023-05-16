2763f86af03c53607df518b10275ecc0f7a84b54
JeonghoPark <pjhpjh0805@naver.com>
2023-05-16 21:55:26 +0900

    feat: add health check controller

--- api-server/src/main/java/com/lastone/apiserver/controller/healthController.java
+++ api-server/src/main/java/com/lastone/apiserver/controller/healthController.java
@@ -1,16 +1,12 @@
 package com.lastone.apiserver.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class TestController {
-    @GetMapping("/api")
-    public void healthCheck() {
-
-
-    }
+public class healthController {
+    @GetMapping("/api/health")
+    public void healthCheck() {}
 }
