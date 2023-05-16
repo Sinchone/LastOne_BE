983a41ae6de18a0114e530ba85e8834c86cf24de
JeonghoPark <pjhpjh0805@naver.com>
2023-05-16 22:15:15 +0900

    feat: edit health.sh

--- api-server/src/main/java/com/lastone/apiserver/controller/HealthController.java
+++ api-server/src/main/java/com/lastone/apiserver/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class healthController {
+public class HealthController {
     @GetMapping("/api/health")
     public void healthCheck() {}
 }
