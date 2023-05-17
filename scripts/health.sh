b61c9be7cbd1831405725eb235896bb690d984a5
JeonghoPark <pjhpjh0805@naver.com>
2023-05-17 18:57:42 +0900

    feat: edit health.sh

--- scripts/health.sh
+++ scripts/health.sh
@@ -22,7 +22,7 @@ for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
 do
     echo "> #${RETRY_COUNT} 번쨰 시도중입니다."
     # 테스트할 API 주소를 통해 http 상태 코드 가져오기 -s -o /dev/null은 출력 및 로그 저장하지 않는 것을 의미
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  https://last-one.xyz/api/health)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://3.38.5.26:${TARGET_PORT}/api/health)
 
 	# RESPONSE_CODE의 http 상태가 200번인 경우 (10번 시도 하는 동안 200 코드가 와야한다)
     if [ ${RESPONSE_CODE} -eq 200 ]; then
