914c0e2963da757987409da6e980745cc940ac6b
JeonghoPark <pjhpjh0805@naver.com>
2023-05-17 20:34:14 +0900

    feat: edit switch.sh

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -18,7 +18,7 @@ fi
 # 위 커맨드들을 통해 현재 타겟포트 가져오기
 
 # $ service_url.inc 파일을 현재 바뀐 서버의 포트로 변경
-echo "set \$service_url http://3.38.5.26:${TARGET_PORT};" | sudo tee /etc/nginx/conf.d/api_service_url.inc
+echo "set \$service_url http://3.38.5.26:${TARGET_PORT}" | sudo tee /etc/nginx/conf.d/api_service_url.inc
 
 echo "> ngnix가 ${TARGET_PORT} 포트 번호를 가르키고 있습니다.."
 
