68fb26c74bdf8a71884725144705249bb8e9b95e
JeonghoPark <pjhpjh0805@naver.com>
2023-06-18 13:46:02 +0900

    [LAS-23] feat: applicationRepository 신청 상태 관련 쿼리 누락 부분 추가

--- core/src/main/java/com/lastone/core/repository/application/ApplicationRepositoryImpl.java
+++ core/src/main/java/com/lastone/core/repository/application/ApplicationRepositoryImpl.java
@@ -19,7 +19,9 @@ import static com.lastone.core.domain.recruitment.QRecruitment.recruitment;
 public class ApplicationRepositoryImpl implements ApplicationRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
+
     private final EntityManager em;
+
     private static final int ONE_DAY = 1;
 
     @Override
@@ -57,6 +59,7 @@ public class ApplicationRepositoryImpl implements ApplicationRepositoryCustom{
                             application.applicant.nickname,
                             application.applicant.profileUrl,
                             application.applicant.gender,
+                            application.status,
                             application.createdAt))
                     .from(application)
                     .where(application.recruitment.id.eq(recruitmentId))
@@ -126,7 +129,7 @@ public class ApplicationRepositoryImpl implements ApplicationRepositoryCustom{
                 .where(
                         application.recruitment.id.eq(recruitmentId),
                         application.id.ne(successApplicationId),
-                        application.status.ne(ApplicationStatus.CANCLE))
+                        application.status.ne(ApplicationStatus.CANCEL))
                 .execute();
 
         em.flush();
