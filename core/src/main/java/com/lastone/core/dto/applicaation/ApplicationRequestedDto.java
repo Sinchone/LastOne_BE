8a2907309452c04a0f97f1e53af51d23363cf5b1
JeonghoPark <pjhpjh0805@naver.com>
2023-06-18 14:38:17 +0900

    [LAS-46] feat: 요청한 신청 응답 데이터에 신청 상태 관련 필드 추가

--- core/src/main/java/com/lastone/core/dto/applicaation/ApplicationRequestedDto.java
+++ core/src/main/java/com/lastone/core/dto/applicaation/ApplicationRequestedDto.java
@@ -3,6 +3,7 @@ package com.lastone.core.dto.applicaation;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.lastone.core.domain.application.ApplicationStatus;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Getter;
 import lombok.ToString;
@@ -14,16 +15,27 @@ import java.time.LocalDateTime;
 public class ApplicationRequestedDto {
 
     private final Long applicationId;
+
     private final Long recruitmentId;
+
     private final String title;
+
     private final String gym;
+
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd 'T' HH:mm")
     private final LocalDateTime startedAt;
+
     private final Long memberId;
+
     private final String profileUrl;
+
     private final String nickname;
+
     private final String gender;
+
+    private final ApplicationStatus status;
+
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd 'T' HH:mm")
     private LocalDateTime applicationDate;
@@ -31,7 +43,8 @@ public class ApplicationRequestedDto {
     @QueryProjection
     public ApplicationRequestedDto(Long applicationId, Long recruitmentId, String title, String gym,
                                    LocalDateTime startedAt, Long memberId, String profileUrl,
-                                   String nickName, String gender, LocalDateTime applicationDate) {
+                                   String nickName, String gender, ApplicationStatus status,
+                                   LocalDateTime applicationDate) {
         this.applicationId = applicationId;
         this.recruitmentId = recruitmentId;
         this.title = title;
@@ -41,6 +54,7 @@ public class ApplicationRequestedDto {
         this.profileUrl = profileUrl;
         this.nickname = nickName;
         this.gender = gender;
+        this.status = status;
         this.applicationDate = applicationDate;
     }
 }
