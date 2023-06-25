060c5b2f1bd17decd02608eeae39561dffd45e71
JeonghoPark <pjhpjh0805@naver.com>
2023-06-26 01:16:34 +0900

    [LAS-54] feat: 마이페이지 닉네임 중복 검증을 위한 메서드 추가

--- api-server/src/main/java/com/lastone/apiserver/service/mypage/MyPageService.java
+++ api-server/src/main/java/com/lastone/apiserver/service/mypage/MyPageService.java
@@ -2,10 +2,13 @@ package com.lastone.apiserver.service.mypage;
 
 import com.lastone.core.dto.mypage.MyPageDto;
 import com.lastone.core.dto.mypage.MyPageUpdateDto;
+import com.lastone.core.dto.mypage.NicknameCheckDto;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 public interface MyPageService {
+
+    NicknameCheckDto isDuplicatedNickname(String nickname);
 
     MyPageDto getMyPage(Long memberId);
 
