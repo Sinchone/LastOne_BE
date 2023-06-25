27161f372cc37544e95a9c3cbaa224c710b25e7a
JeonghoPark <pjhpjh0805@naver.com>
2023-06-26 01:13:50 +0900

    [LAS-54] feat: 닉네임 중복 검증 결과 반환을 위한 dto생성

---
+++ core/src/main/java/com/lastone/core/dto/mypage/NicknameCheckDto.java
@@ -0,0 +1,11 @@
+package com.lastone.core.dto.mypage;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class NicknameCheckDto {
+
+    private Boolean isDuplicated;
+}
